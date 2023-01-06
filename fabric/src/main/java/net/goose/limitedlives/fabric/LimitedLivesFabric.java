dee1e05b9b38997c92a165f2c16b6eb86bc047ca
loxigoose <docgoosenn@gmail.com>
2023-01-05 20:32:49 -0800

    Fixed commands not being registered on Fabric

--- fabric/src/main/java/net/goose/limitedlives/fabric/LimitedLivesFabric.java
+++ fabric/src/main/java/net/goose/limitedlives/fabric/LimitedLivesFabric.java
@@ -4,6 +4,7 @@ import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
 import net.goose.limitedlives.LimitedLives;
 import net.fabricmc.api.ModInitializer;
 import net.goose.limitedlives.configuration.ConfigHolder;
+import net.goose.limitedlives.event.ModCommands;
 import net.minecraftforge.fml.config.ModConfig;
 
 public class LimitedLivesFabric implements ModInitializer {
@@ -12,5 +13,6 @@ public class LimitedLivesFabric implements ModInitializer {
         ForgeConfigRegistry.INSTANCE.register(LimitedLives.MOD_ID, ModConfig.Type.COMMON, ConfigHolder.SERVER_SPEC);
         LimitedLives.config = ConfigHolder.SERVER;
         LimitedLives.init();
+        ModCommands.register();
     }
 }
