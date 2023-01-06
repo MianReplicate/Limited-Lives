a44c63d6963abed3a2def20760ea001e4ed08e3a
loxigoose <docgoosenn@gmail.com>
2023-01-05 23:41:15 -0800

    1.19.2 complete

--- fabric/src/main/java/net/goose/limitedlives/fabric/LimitedLivesFabric.java
+++ fabric/src/main/java/net/goose/limitedlives/fabric/LimitedLivesFabric.java
@@ -1,16 +1,16 @@
 package net.goose.limitedlives.fabric;
 
-import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
 import net.goose.limitedlives.LimitedLives;
 import net.fabricmc.api.ModInitializer;
 import net.goose.limitedlives.configuration.ConfigHolder;
 import net.goose.limitedlives.event.ModCommands;
+import net.minecraftforge.api.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
 public class LimitedLivesFabric implements ModInitializer {
     @Override
     public void onInitialize() {
-        ForgeConfigRegistry.INSTANCE.register(LimitedLives.MOD_ID, ModConfig.Type.COMMON, ConfigHolder.SERVER_SPEC);
+        ModLoadingContext.registerConfig(LimitedLives.MOD_ID, ModConfig.Type.COMMON, ConfigHolder.SERVER_SPEC);
         LimitedLives.config = ConfigHolder.SERVER;
         LimitedLives.init();
         ModCommands.register();
